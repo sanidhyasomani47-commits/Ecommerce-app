358e143f7de6619c1b70cac51ff64508f1c5e147
Sanidhya Somani <sanidhyasomani47@gmail.com>
2026-03-12 00:20:29 +0530

    update

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,26 @@
 import mongoose from "mongoose";
-const connectdb=async()=>{
-        mongoose.connection.on('connected',()=>{
-            console.log("DB Connected")
-        })
 
-        await mongoose.connect(`${process.env.MONGODB_URL}/e-commerce`)
+// Global cache for serverless environments (Vercel)
+let cachedConnection = global.mongoose;
+if (!cachedConnection) {
+  cachedConnection = global.mongoose = { conn: null, promise: null };
+}
+
+const connectdb = async () => {
+    if (cachedConnection.conn) return cachedConnection.conn;
+
+    if (!cachedConnection.promise) {
+        mongoose.connection.on('connected', () => {
+            console.log("DB Connected");
+        });
+
+        cachedConnection.promise = mongoose.connect(`${process.env.MONGODB_URL}/e-commerce`).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    cachedConnection.conn = await cachedConnection.promise;
+    return cachedConnection.conn;
 }
 
 export default connectdb;
