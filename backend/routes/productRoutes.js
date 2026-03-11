9e45deee16487d88c2d4c8e6a23cc8892c2c8241
Sanidhya Somani <sanidhyasomani47@gmail.com>
2026-03-11 23:20:17 +0530

    update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ import { listProducts, addProduct, removeProduct, singleProduct } from '../contr
 import upload from '../middleware/multer.js';
 import adminAuth from '../middleware/adminauth.js';
 
+
 const productRouter = express.Router();
 
 productRouter.post('/add',adminAuth, upload.fields([{ name: "image1", maxCount: 1 }, { name: "image2", maxCount: 1 }, { name: "image3", maxCount: 1 }, { name: "image4", maxCount: 1 }]), addProduct);
