9d830907fabc62c6c538d0c0422ade9efb47e7f0
Sanidhya Somani <sanidhyasomani47@gmail.com>
2026-03-12 20:35:51 +0530

    update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,39 +1,51 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { ShopContext } from "../context/ShopContext";
 function Footer() {
+  const { navigate } = useContext(ShopContext);
   return (
     <div>
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14  my-10 mt-40 text-sm'>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14  my-10 mt-40 text-sm">
         <div>
-            <img src={assets.logo} className='mb-5 w-32' alt="" />
-            <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ratione autem impedit voluptatem eaque? Ad numquam eos libero esse rem.</p>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+            ratione autem impedit voluptatem eaque? Ad numquam eos libero esse
+            rem.
+          </p>
         </div>
         <div>
-            <p className='text-xl font-medium mb-5'>COMPANY</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
-            </ul>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li onClick={() => navigate("/")} className="cursor-pointer">
+              Home
+            </li>
+
+            <li onClick={() => navigate("/about")} className="cursor-pointer">
+              About us
+            </li>
+
+            <li onClick={() => navigate("/#policy")} className="cursor-pointer">
+              Policy
+            </li>
+          </ul>
         </div>
-           <div>
-            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+1-121-444-7176</li>
-                <li>contact@foraveryou.com</li>
-            </ul>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-121-444-7176</li>
+            <li>contact@foraveryou.com</li>
+          </ul>
         </div>
-        
       </div>
-     <div>
-            <hr/>
-            <p className='py-4 text-sm text-center'>Copyright {new Date().getFullYear()} forever.com - All Rights Reserved</p>
-        </div>
+      <div>
+        <hr />
+        <p className="py-4 text-sm text-center">
+          Copyright {new Date().getFullYear()} forever.com - All Rights Reserved
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 function OurPolicy() {
   return (
-    <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
+    <div id="policy" className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
         <div>
             <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt=''/>
             <p className='font-semibold'>Easy Exchange Policy</p>
